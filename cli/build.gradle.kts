f71c0b90fc6359d4eadc21bc11fe02b5b78bce4a
Kevin Galligan <kgalligan@gmail.com>
2024-05-20 15:37:06 -0400

    Import

---
+++ cli/build.gradle.kts
@@ -0,0 +1,59 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+val GROUP: String by project
+val VERSION_NAME: String by project
+
+group = GROUP
+version = VERSION_NAME//.${(System.currentTimeMillis()/1000)}"
+
+kotlin {
+    listOf(macosX64(), macosArm64()).forEach {
+        it.binaries {
+            executable {
+                entryPoint = "co.touchlab.gitportal.main"
+
+                runTask?.run {
+                    val args = providers.gradleProperty("runArgs")
+                    args(args.getOrElse("").split(' '))
+
+                    standardOutput = System.out
+                    errorOutput = System.err
+                }
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.cli)
+                implementation(libs.kermit)
+                implementation(libs.okio)
+                implementation(libs.uuid)
+            }
+        }
+
+        val macosX64Main by getting {
+            dependencies {
+                implementation(files("macosX64/main/klib/cli.klib"))
+            }
+        }
+
+        val macosArm64Main by getting {
+            dependencies {
+                implementation(files("macosArm64/main/klib/cli.klib"))
+            }
+        }
+
+        all {
+            languageSettings.optIn("kotlinx.cli.ExperimentalCli")
+            languageSettings.optIn("kotlinx.cinterop.BetaInteropApi")
+            languageSettings.optIn("kotlin.experimental.ExperimentalNativeApi")
+        }
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "GitPortal"
+
+include(":core")
+//include(":bidir")
+include(":cli")
+include(":cli-files")
