51943e2baf00fe7aaf8ba847f4ec5a517a49b606
Kevin Galligan <kgalligan@gmail.com>
2024-06-07 10:57:56 -0400

    Version 0.6.13

--- cli-build/src/macosArm64Main/kotlin/MainCli.macosArm64.kt
+++ cli-build/src/macosArm64Main/kotlin/MainCli.macosArm64.kt
@@ -1,9 +1,11 @@
 import co.touchlab.gitportal.Main
+import okio.Path.Companion.toPath
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class MainCli actual constructor() {
     actual fun entry(args: Array<String>, vararg commands: (Any) -> Any) {
         Main().entry(
+            ".".toPath(),
             if (args.isEmpty()) {
                 arrayOf("--help")
             } else {
