d19d51167d43acfb1d955f9f7370e38432ea7f64
Kevin Galligan <kgalligan@gmail.com>
2024-06-12 10:55:02 -0400

    Version 0.6.28

--- cli-build/uploaddsyms-arm.sh
+++ cli-build/uploaddsyms-arm.sh
@@ -1,4 +1,3 @@
-brew install getsentry/tools/sentry-cli
 sentry-cli debug-files upload --auth-token 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 \
   --include-sources \
   --org touchlab \
